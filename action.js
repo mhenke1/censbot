994e0f75a9e6d48d2520d3aea7c1553c82b16e02
martin-henke <martin.henke@de.ibm.com>
2020-07-07 12:16:22 +0200

    Move to Cloud Functions

---
+++ action.js
@@ -0,0 +1,49 @@
+import validator from 'validator';
+import UuidEncoder from 'uuid-encoder';
+const base36Encoder = new UuidEncoder('base36');
+
+'use strict';
+
+function convertId(params) {
+
+  const reuqestUUID = params.text.trim();
+  console.log(reuqestUUID);
+
+  let responseText = 'No able to convert the given input `' + reuqestUUID + '`';
+
+  if (validator.isUUID(reuqestUUID)) {
+    const base36UUID = base36Encoder.encode(reuqestUUID);
+    responseText = 'The namespace name for the UUID `' + reuqestUUID + '` is `' + base36UUID + '`';
+  }
+  else if (reuqestUUID.length <= 25) {
+    const longUUID = base36Encoder.decode(reuqestUUID);
+    if (validator.isUUID(longUUID)) {
+      responseText = 'The UUID for the namespace `' + reuqestUUID + '` is `' + longUUID + '`';
+    } else {
+      responseText = 'No able to convert the namespace name `' + reuqestUUID + '` to a valid UUID';
+    }
+  }
+
+  const responseJSON = {
+    'blocks': [
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': responseText,
+        },
+      },
+      {
+        'type': 'section',
+        'text': {
+          'type': 'mrkdwn',
+          'text': '_censbot is powered by_ :cloudfunctions-whitecircle: _IBM Cloud Functions_',
+        },
+      },
+    ],
+  };
+
+  return responseJSON;
+}
+
+global.main = convertId;
